a8ab714748427c859136727219371915ae5293a5
Daniel Nevius <dmneviusthemouse@gmail.com>
2017-01-19 23:06:52 -0500

    Narwhal 0.1.0
    - Initial commit
    - Plaintext only for now

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * ============= NARWHAL =============
+ *    A web browser for the terminal
+ * ===================================
+ */
+
+'use strict';
+
+const Narwhal = require('./lib');
+const plaintext = require('./lib/mode/plaintext');
+
+const browser = new Narwhal.Browser();
+browser.navigate(process.argv[2]).then((dom) => {
+  try {
+    console.log(plaintext(dom));
+  } catch (e) {
+    throw e;
+  }
+}).catch((error) => {
+  console.log(error);
+});
