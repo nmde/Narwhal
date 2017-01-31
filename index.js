ecca676cf80863acf3e4d5f309b9c413092e1208
Daniel Nevius <dmneviusthemouse@gmail.com>
2017-01-31 18:56:18 -0500

    Typo fixes

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-=======
-/**
- * ============= NARWHAL =============
- *    A web browser for the terminal
- * ===================================
- */
-
->>>>>>> 2ec463d87175c1f51f6d1ef293f5362fcb3ce443
 'use strict';
 
 const Narwhal = require('./lib');
 
-<<<<<<< HEAD
-const window = new Narwhal.Window('Narwhal');
-=======
-const browser = new Narwhal.Browser();
-browser.navigate(process.argv[2]).then(() => {}).catch((error) => {
-  console.log(error);
-});
->>>>>>> 2ec463d87175c1f51f6d1ef293f5362fcb3ce443
+const window = new Narwhal.Window();
+console.log(window.document.createElement('div'));
