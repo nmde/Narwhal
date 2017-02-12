6a76dbd84a0942fb5daff307cbea7c9016e05760
Daniel Nevius <dmneviusthemouse@gmail.com>
2017-02-11 19:06:35 -0500

    Narwhal v0.7.2
    - Changing document.title now automatically updates the screen title
    - Added "watch" util for watching variable changes

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const Narwhal = require('./lib');
 const window = new Narwhal.Window();
 const div = window.document.createElement('div');
 div.setAttribute('class', 'foo');
+window.document.title = 'Foo';
 window.render();
+
+setTimeout(() => {
+  window.document.title = 'Bar';
+}, 5000);
