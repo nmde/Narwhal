d0fcb0350451224954a3bfe9610311bbd79e369e
Daniel Nevius <dmneviusthemouse@gmail.com>
2017-02-14 12:21:45 -0500

    Old repository was corrupted

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Narwhal = require('./lib');
+
+const window = new Narwhal.Window();
+const div = window.document.createElement('div');
+div.setAttribute('class', 'foo');
+window.document.appendChild(div);
+window.document.title = 'Foo';
+window.render();
+
+setTimeout(() => {
+  window.document.title = 'Bar';
+}, 5000);
