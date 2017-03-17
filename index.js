25e9aefd0a97c4e2b94478008edd3910b9d5206d
Daniel Nevius <dmneviusthemouse@gmail.com>
2017-03-17 18:07:27 -0400

    Narwhal v0.10.0
    - Added a Browser class
    - Added an HTML parser

--- index.js
+++ index.js
@@ -2,14 +2,5 @@
 
 const Narwhal = require('./lib');
 
-const window = new Narwhal.Window();
-window.document.title = 'Foo';
-window.render();
-
-setTimeout(() => {
-  window.document.title = 'Bar';
-}, 5000);
-
-setInterval(() => {
-  window.screen.createBox();
-}, 1000);
+const browser = new Narwhal.Browser();
+console.log(browser.open('<div class="foo"></div>'));
